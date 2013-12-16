d64932a82b99b75daae8fb43d98fb6a48a7a9177
Paul Friedman <p-friedman@northwestern.edu>
2013-12-16 14:26:37 -0600

    named_scope is now just scope #10505
    The named_scope method has been renamed to just scope.
    More information: http://github.com/rails/rails/commit/d60bb0a9e4be2ac0a9de9a69041a4ddc2e0cc914

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -94,82 +94,82 @@ class Submission < ActiveRecord::Base
 
   accepts_nested_attributes_for :applicant
 
-  default_scope :joins => [:applicant], :order => 'lower(users.last_name), submissions.project_id, lower(submissions.submission_title)'
-  named_scope :assigned_submissions, :conditions => ['submission_reviews_count >= 2']
-  named_scope :unfilled_submissions, lambda { |*args| {:conditions => [ 'submission_reviews_count < :max_reviewers', {:max_reviewers => args.first || 2 } ] } }
+  default_scope joins([:applicant]).order('lower(users.last_name), submissions.project_id, lower(submissions.submission_title)')
+  scope :assigned_submissions, where('submission_reviews_count >= 2')
+  scope :unfilled_submissions, lambda { |*args| where('submission_reviews_count < :max_reviewers', { :max_reviewers => args.first || 2 }) }
 
-  named_scope :unassigned_submissions, :conditions => {:submission_reviews_count => 0}
-  named_scope :recent, lambda { { :conditions => ['submissions.created_at > ?', 3.weeks.ago] } }
+  scope :unassigned_submissions, where(:submission_reviews_count => 0)
+  scope :recent, lambda { where('submissions.created_at > ?', 3.weeks.ago) }
 
-  named_scope :user_scoped, lambda { |*args|
+  scope :user_scoped, lambda { |*args|
     if args.first
-      {:include=>[:key_people, :applicant, :project, :submitter, :effort_approver, :department_administrator, :core_manager ]}
+      includes([:key_people, :applicant, :project, :submitter, :effort_approver, :department_administrator, :core_manager])
     elsif args[2].nil?
-      {:conditions => ['applicant_id = :id', {:id => 0} ]}
+      where('applicant_id = :id', { :id => 0 })
     else
-      {:conditions => ['(applicant_id = :id or created_id = :id) and project_id IN (:projects)', {:projects => args[1], :id => args[2]} ] } 
+      where('(applicant_id = :id or created_id = :id) and project_id IN (:projects)', { :projects => args[1], :id => args[2] })
     end
   }
-  
-  named_scope :associated, lambda { |*args|
+
+  scope :associated, lambda { |*args|
     if args[1].nil?
-      {:conditions => ['applicant_id = :id', {:id => 0} ]}
+      where('applicant_id = :id', { :id => 0 })
     else
-      { :include => 'submission_reviews',
-       :conditions => ['(submissions.applicant_id = :id or submissions.created_id = :id) and submissions.project_id IN (:projects)', {:projects => args[0], :id => args[1]} ] } 
+      includes('submission_reviews')
+      .where('(submissions.applicant_id = :id or submissions.created_id = :id) and submissions.project_id IN (:projects)', { :projects => args[0], :id => args[1] })
     end
   }
-    
-  named_scope :associated_with_user, lambda { |*args|
+
+  scope :associated_with_user, lambda { |*args|
     if args.first.nil?
-      {:conditions => ['applicant_id = :id', {:id => 0} ]}
+      where('applicant_id = :id', { :id => 0 })
     else
-      { :include => 'submission_reviews', 
-        :conditions => ['submissions.applicant_id = :id or submissions.created_id = :id', {:id => args.first} ], 
-        :order => "id asc" } 
+      includes('submission_reviews')
+      .where('submissions.applicant_id = :id or submissions.created_id = :id', { :id => args.first })
+      .order('id asc')
     end
   }
-  
+
   attr_accessor :max_budget_request
   attr_accessor :min_budget_request
-  
+
   before_validation :clean_params, :set_defaults
-  
+
   validates_length_of :submission_title, :within => 6..81, :too_long => "--- pick a shorter title", :too_short => "--- pick a longer title"
   validates_numericality_of :direct_project_cost, :greater_than => 1000000, :if => Proc.new { |sub| (sub.direct_project_cost || 0) < sub.min_project_cost && ! sub.direct_project_cost.blank?  }, :message => "is too low"
   validates_numericality_of :direct_project_cost, :less_than => 1000, :if => Proc.new { |sub| (sub.direct_project_cost || 0) > sub.max_project_cost }, :message => "is too high"
-  
-  def overall_scores 
+
+  def overall_scores
     return 0 if submission_reviews.length == 0
     cnt = submission_reviews.collect{ |s| s.z?(s.overall_score) ? 0 : 1 }.sum
     return 0 if cnt < 1
     submission_reviews.collect{ |s| s.z?(s.overall_score) ? 0 : s.overall_score }.sum/cnt
-  end 
+  end
   def overall_scores_string
     return 0 if submission_reviews.length == 0
     overall_scores.to_s + " : " + submission_reviews.collect(&:overall_score).join(" / ")
-  end 
-  def composite_scores 
+  end
+  def composite_scores
     return 0 if submission_reviews.length == 0
     cnt = submission_reviews.collect{ |s| s.has_zero? ? 0 : 1 }.sum
     return 0 if cnt < 1
     submission_reviews.collect{ |s| s.has_zero? ? 0 : s.composite_score }.sum/cnt
-  end 
-  
+  end
+
   def composite_scores_string
     return 0 if submission_reviews.length == 0
     composite_scores.to_s + " : " + submission_reviews.collect(&:composite_score).join(" / ")
-  end 
-  
-  def max_project_cost 
-    max_budget_request || 50000
-  end 
-  
-  def min_project_cost 
-    min_budget_request || 1000
-  end 
+  end
 
-  def status 
+  def max_project_cost
+    max_budget_request || 50000
+  end
+
+  def min_project_cost
+    min_budget_request || 1000
+  end
+
+  def status
     return "Incomplete" if project.blank? or applicant.blank?
     return "Incomplete" if project.show_project_cost and direct_project_cost.blank?
     return "Incomplete" if project.show_effort_approver and effort_approver_username.blank?
@@ -185,8 +185,8 @@ class Submission < ActiveRecord::Base
     return "Incomplete" if project.show_manage_biosketches and applicant_biosketch_document_id.blank?
     return "Incomplete" if project.show_application_doc and application_document_id.blank?
     "Complete"
-  end 
-  
+  end
+
   def is_complete?
     self.status == 'Complete'
   end
@@ -198,7 +198,7 @@ class Submission < ActiveRecord::Base
   def key_personnel_emails
     self.key_personnel.map{|k| k.email|| k.user.email}
   end
- 
+
   def status_reason
     out=[]
     return ["Project undefined"] if project.blank? or applicant.blank?
@@ -217,67 +217,67 @@ class Submission < ActiveRecord::Base
     out << "Application document needs to be uploaded. "  if project.show_application_doc and application_document_id.blank?
     out << "Application has been fully completed!" if out.blank?
     out.compact
-  end 
+  end
 
   def self.approved_submissions(username)
-    self.all( :conditions=> ['effort_approver_username = :username',{:username => username} ] )
+    self.where('effort_approver_username = :username', { :username => username }).all
   end
   def uploaded_biosketch=(data_field)
     # this will update the applicant's personal biosketch and then add to the submission as well
     # set the current biosketch
     unless data_field.blank?
       self.applicant_biosketch_document = FileDocument.new if self.applicant_biosketch_document.nil?
-      self.applicant_biosketch_document.uploaded_file = data_field 
+      self.applicant_biosketch_document.uploaded_file = data_field
       # do not update the applicant's biosketch
-      # self.applicant.uploaded_biosketch = data_field 
+      # self.applicant.uploaded_biosketch = data_field
     end
-  end 
- 
-  # this defines the connection between the model attribute exposed to the form (uploaded_budget ) 
+  end
+
+  # this defines the connection between the model attribute exposed to the form (uploaded_budget )
   # and the file_document model
-  def uploaded_budget=(data_field) 
+  def uploaded_budget=(data_field)
     self.budget_document = FileDocument.new if self.budget_document.nil?
     self.budget_document.uploaded_file = data_field unless data_field.blank?
-  end 
+  end
 
-  def uploaded_other_support=(data_field) 
+  def uploaded_other_support=(data_field)
     self.other_support_document = FileDocument.new if self.other_support_document.nil?
     self.other_support_document.uploaded_file = data_field unless data_field.blank?
-  end 
+  end
 
-  def uploaded_document1=(data_field) 
+  def uploaded_document1=(data_field)
     self.document1 = FileDocument.new if self.document1.nil?
     self.document1.uploaded_file = data_field unless data_field.blank?
-  end 
+  end
 
-  def uploaded_document2=(data_field) 
+  def uploaded_document2=(data_field)
     self.document2 = FileDocument.new if self.document2.nil?
     self.document2.uploaded_file = data_field unless data_field.blank?
-  end 
+  end
 
-  def uploaded_document3=(data_field) 
+  def uploaded_document3=(data_field)
     self.document3 = FileDocument.new if self.document3.nil?
     self.document3.uploaded_file = data_field unless data_field.blank?
-  end 
+  end
 
-  def uploaded_document4=(data_field) 
+  def uploaded_document4=(data_field)
     self.document4 = FileDocument.new if self.document4.nil?
     self.document4.uploaded_file = data_field unless data_field.blank?
-  end 
+  end
 
-  # this defines the connection between the model attribute exposed to the form (uploaded_application ) 
+  # this defines the connection between the model attribute exposed to the form (uploaded_application )
   # and the storage fields for the file
-  def uploaded_application=(data_field) 
+  def uploaded_application=(data_field)
     self.application_document = FileDocument.new if self.application_document.nil?
     self.application_document.uploaded_file = data_field unless data_field.blank?
-  end 
-  
+  end
+
   def clean_params
     # need the before_type_cast or else Rails 2.3 truncates after any comma. strange
     txt = self.direct_project_cost_before_type_cast
     return if txt.blank?
     txt = txt.to_s
-    txt = txt.split('.')[0] 
+    txt = txt.split('.')[0]
     txt = txt.split(',').join
     txt = txt.sub(/\D+(\d*)/,'\1')
     self.direct_project_cost=txt
@@ -287,7 +287,7 @@ class Submission < ActiveRecord::Base
       self.submission_status='Pending'
     end
   end
-  
+
   def save_documents
     do_save(self.budget_document, "budget document")
     do_save(self.other_support_document, "other support document")
@@ -299,7 +299,7 @@ class Submission < ActiveRecord::Base
     set_applicant_biosketch()
     do_save(self.applicant_biosketch_document, "pi biosketch document")
   end
-  
+
   def set_applicant_biosketch()
     unless self.applicant.blank? or self.applicant.biosketch_document_id.blank?
       if self.applicant_biosketch_document_id.blank?
@@ -317,11 +317,11 @@ class Submission < ActiveRecord::Base
       end
     end
   end
-  
-  def do_save(model, name="document")
+
+  def do_save(model, name = "document")
     if !model.nil? and model.changed?
       if model.errors.blank?
-        model.save 
+        model.save
       end
       unless model.errors.blank?
         self.errors.add "unable to save #{name}: " + model.errors.full_messages.join("; ")
--- app/models/user.rb
+++ app/models/user.rb
@@ -48,7 +48,7 @@
 #
 
 class User < ActiveRecord::Base
-  
+
   has_many :reviewers  #really program reviewers since the reviewer model is a user + program
   belongs_to :biosketch, :class_name => "FileDocument", :foreign_key => 'biosketch_document_id'
   has_many :key_personnel
@@ -57,34 +57,34 @@ class User < ActiveRecord::Base
   has_many :roles_users
   has_many :roles, :through => :roles_users
   has_many :logs
-  
+
   has_many :submissions, :foreign_key => 'applicant_id'
   has_many :proxy_submissions, :class_name => "Submission", :foreign_key => 'created_id'
   attr_accessor :validate_for_applicant
   attr_accessor :validate_era_commons_name
   attr_accessor :validate_name
   attr_accessor :validate_email_attr
-  
+
   after_save :save_documents
 
-  named_scope :project_applicants, lambda { |*args| {:joins=>[:submissions], :conditions => ['submissions.project_id IN (:project_ids)', {:project_ids => args.first} ] }}
+  scope :project_applicants, lambda { |*args| joins([:submissions]).where('submissions.project_id IN (:project_ids)', { :project_ids => args.first }) }
 
-  default_scope :order => 'lower(users.last_name),lower(users.first_name)'
-  named_scope :program_reviewers, lambda { |*args| {:joins=>:reviewers, :conditions => ['reviewers.program_id = :program_id', {:program_id => args.first} ] }}
-  
+  default_scope order('lower(users.last_name),lower(users.first_name)')
+  scope :program_reviewers, lambda { |*args| joins(:reviewers).where('reviewers.program_id = :program_id', { :program_id => args.first }) }
+
   validates_presence_of :username
   validates_uniqueness_of :username
+  validates_presence_of :first_name, :if => :validate_first_last
+  validates_presence_of :last_name, :if => :validate_first_last
   validates_presence_of :era_commons_name, :if => :validate_era_commons
   validates_uniqueness_of :era_commons_name, :if => :validate_era_commons
   validates_presence_of :email, :if => :validate_email
   validates_uniqueness_of :email, :if => :validate_email
-  validates_format_of :email, 
-    :with => %r{^[a-zA-Z0-9\.\-\_][a-zA-Z0-9\.\-\_]+@[^\.]+\..+$}i, 
-    :message => "Email address is not valid. Please correct",
-    :if => Proc.new { |c| !c.email.blank? }
-  validates_presence_of :first_name, :if => :validate_first_last
-  validates_presence_of :last_name, :if => :validate_first_last
-  
+  validates_format_of :email,
+                      :with => %r{^[a-zA-Z0-9\.\-\_][a-zA-Z0-9\.\-\_]+@[^\.]+\..+$}i,
+                      :message => "Email address is not valid. Please correct",
+                      :if => Proc.new { |c| !c.email.blank? }
+
   def name
     [first_name, last_name].join(' ').gsub(/\'/, "’")
   end
@@ -104,29 +104,29 @@ class User < ActiveRecord::Base
   def long_name
     [name, degrees].compact.join(", ").gsub(/, +$/,"").gsub(/\'/, "’")
   end
-  
-  def validate_first_last 
+
+  def validate_first_last
     validate_name || validate_name.nil?
-  end 
+  end
 
-  def validate_email 
+  def validate_email
     (validate_email_attr == false) ? false : true
-  end 
+  end
 
-  def validate_era_commons 
+  def validate_era_commons
     validate_era_commons_name
-  end 
-  
-  # this defines the connection between the model attribute exposed to the form (uploaded_photo ) 
+  end
+
+  # this defines the connection between the model attribute exposed to the form (uploaded_photo )
   # and the storage fields for the file
-  def uploaded_photo=(field) 
+  def uploaded_photo=(field)
     self.photo = FileDocument.new if self.photo.nil?
     self.photo.uploaded_file = field
-  end 
+  end
 
   def uploaded_biosketch=(field)
     if self.biosketch_document_id.nil?
-      self.biosketch = FileDocument.new 
+      self.biosketch = FileDocument.new
       begin
         logger.warn "creating new biosketch object for investigator"
       rescue
@@ -134,10 +134,10 @@ class User < ActiveRecord::Base
       end
     end
     self.biosketch.uploaded_file = field
-  end 
+  end
 
   def save_documents
     self.biosketch.save if !self.biosketch.nil? and self.biosketch.changed?
-  end 
-  
+  end
+
 end
