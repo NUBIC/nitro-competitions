253240c5f7b5cce5dd934be0351bc58a474e85a1
Paul Friedman <p-friedman@northwestern.edu>
2014-01-07 15:13:32 -0600

    setup for spec/features #10505

---
+++ spec/features/public_spec.rb
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe 'Public Section' do
+
+  context 'for a person who has not logged in' do
+
+    describe 'visiting the welcome page' do
+      before do
+        visit welcome_path
+      end
+
+      it 'shows the home page' do
+        expect(page).to have_content('NUCATS Competition and Awards Application Site')
+        expect(page).to have_content('Welcome to NUCATS Assist')
+        expect(page).to have_content('Listing of current and recent competitions')
+      end
+    end
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,18 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+# For front-end testing with phantomjs and poltergeist
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, js_errors: true, inspector: true)
+end
+Capybara.javascript_driver = :poltergeist
+
+require 'shoulda'
+require 'factory_girl'
+
 module TestLogins
   def user_login
     Aker.authority.valid_credentials?(:user, 'chisholm', 'chisholm')
@@ -18,7 +30,6 @@ module TestLogins
     controller.request.env['aker.check'] = Aker::Rack::Facade.new(Aker.configuration, as)
   end
 end
-
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -34,12 +45,12 @@ RSpec.configure do |config|
   # config.mock_with :rr
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
