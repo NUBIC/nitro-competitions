9b6160dadebfb9d2fbc91d5242caf3703c3db43e
Paul Friedman <p-friedman@northwestern.edu>
2014-03-20 15:06:51 -0500

    Fixed issue with key personnel link #10871
    The remove link was not working and the add link
    in development was adding two key personnel forms.
    
    The development issue was due to locally pre-compiling assets.
    
    The remove link has the :remote => true option outside
    of the html_options hash.
    
    Fixes #10871

--- app/controllers/key_personnel_controller.rb
+++ app/controllers/key_personnel_controller.rb
@@ -4,30 +4,31 @@ class KeyPersonnelController < ApplicationController
 
   # for rjs call on form
   def lookup
-    dom_id = 'key_personnel_'+params[:index]
+    dom_id = 'key_personnel_' + params[:index]
     begin
-      raise NameError, "set username" if params[:username].blank?
-      raise NameError, "set submission_id" if params[:submission_id].nil?
-      raise NameError, "set role" if params[:role].nil?
-      raise NameError, "set id" if params[:index].nil?
-      applicant = User.new(:username=>params[:username])
+      raise NameError, 'set username' if params[:username].blank?
+      raise NameError, 'set submission_id' if params[:submission_id].nil?
+      raise NameError, 'set role' if params[:role].nil?
+      raise NameError, 'set id' if params[:index].nil?
+      applicant = User.new(username: params[:username])
       submission = Submission.find(params[:submission_id])
       applicant = handle_ldap(applicant)
       raise StandardError, "<i>#{params[:username]}</i> not found" if applicant.last_name.blank?
-      if applicant.new_record?
-        add_user(applicant)
-      end
+      add_user(applicant) if applicant.new_record?
       key = add_key_person(applicant, params[:submission_id], params[:role])
       render :update do |page|
-        page.replace_html dom_id, :partial=>'shared/personnel_fields', :layout=>false, :locals=>{:key_person=>key, :submission=>submission, :index=>params[:index]}
+        page.replace_html(dom_id,
+                          partial: 'shared/personnel_fields',
+                          layout: false,
+                          locals: { key_person: key, submission: submission, index: params[:index] })
       end
     rescue StandardError => error
       render :update do |page|
-        page.replace_html dom_id+"_username_id", :inline => "<span style='color:green;'>#{error.message}</span>"
+        page.replace_html("#{dom_id}_username_id", :inline => "<span style='color:green;'>#{error.message}</span>")
       end
      rescue Exception => error
        render :update do |page|
-         page.replace_html 'errors', :inline => "<span style='color:red;'>#{error.message}</span>"
+         page.replace_html('errors', :inline => "<span style='color:red;'>#{error.message}</span>")
        end
     end
   end
