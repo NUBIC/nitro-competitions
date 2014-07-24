2a43ab74fd0c6f9dde74b83242a0c879bb93ce38
Paul Friedman <p-friedman@northwestern.edu>
2014-07-24 15:07:58 -0500

    For ORD, use different email address and template
    Send from ord-nu@northwestern.edu
    and have a custom template with pattern
    "#{program.program_name}_message.html.erb"
    
    This should work for now, but going forward we might
    want to consider other configurations for the email
    from address and the email message template.

--- lib/submission_emails.rb
+++ lib/submission_emails.rb
@@ -5,11 +5,7 @@ def send_finalize_email(submission, user)
   if allow_emails? || %w(wakibbe pfr957).include?(user.username)
     before_notify_email(submission)
     begin
-      message = Notifier.finalize_message(Rails.application.config.from_address,
-                                          'Thank you for your NUCATS Assist Submission',
-                                          submission,
-                                          submission_url(submission.id),
-                                          project_url(submission.project.id))
+      message = create_message(submission)
       logger.error("~~~ about to deliver message #{message.inspect}")
       message.deliver
       submission.save!
@@ -37,4 +33,36 @@ def send_finalize_email(submission, user)
     end
   end
   true
+end
+
+def create_message(submission)
+  from = determine_from_address(submission)
+  subject = 'Thank you for your NUCATS Assist Submission'
+
+  path = "#{Rails.root}/app/views/notifier/#{sponsor_name(submission)}_message.html.erb"
+  if File.exists?(path)
+    msg_method = "#{sponsor_name(submission)}_message".to_sym
+    subject = 'Thank you for your submission'
+  else
+    msg_method = :finalize_message
+  end
+  Notifier.send(msg_method,
+                from,
+                subject,
+                submission,
+                submission_url(submission.id),
+                project_url(submission.project.id))
+end
+
+def sponsor_name(submission)
+  submission.program_name.to_s.downcase
+end
+
+def determine_from_address(submission)
+  email_addresses = { 'ord' => 'ord-nu@northwestern.edu' }
+  result = Rails.application.config.from_address
+
+  program_email_address = email_addresses[sponsor_name(submission)]
+  result = program_email_address unless program_email_address.blank?
+  result
 end
