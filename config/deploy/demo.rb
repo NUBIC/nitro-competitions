0eb6b9e28fe98b6c663b0f9cc9c6af71e083a2a1
jefflunt <jefflunt@gmail.com>
2014-12-15 16:26:32 -0600

    ASSIST-63 - Removed need for .env file on demo server.

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -1,9 +1,12 @@
 # -*- encoding: utf-8 -*-
 set :stage, :demo
 set :rails_env, :demo
+set :deploy_to, '/var/www/apps/nitro-competitions'
+set :linked_files, []
 
 role :app, %w{grants-demo.nubic.northwestern.edu}
 role :web, %w{grants-demo.nubic.northwestern.edu}
 role :db,  %w{grants-demo.nubic.northwestern.edu}
 
 server 'grants-demo.nubic.northwestern.edu', user: 'deploy', roles: %w{web app}, primary: true
+
