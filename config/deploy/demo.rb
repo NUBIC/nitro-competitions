3bb2af15969802f915089a258991aece4ac44b8b
jefflunt <jefflunt@gmail.com>
2014-12-16 08:10:41 -0600

    Added Postgres 9.3 flag for building pg gem.

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -3,6 +3,7 @@ set :stage, :demo
 set :rails_env, :demo
 set :deploy_to, '/var/www/apps/nitro-competitions'
 set :linked_files, []
+set :bundle_flags, '--deployment --quiet -- --with-pg-config=/usr/pgsql-9.3/bin/pg_config'
 
 role :app, %w{grants-demo.nubic.northwestern.edu}
 role :web, %w{grants-demo.nubic.northwestern.edu}
