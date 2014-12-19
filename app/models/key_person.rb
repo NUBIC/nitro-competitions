e8ac55dcd1935c83b572a4e01d2c469a0a7bb797
jefflunt <jefflunt@gmail.com>
2014-12-19 13:44:46 -0600

    ASSIST-80 More focused tests for KeyPerson
    - Still unclear if :user and :submission can be nil and the record still
      be valid. I found cases of both of these being nil in production, and
      sent Paul and email asking about it.

--- app/models/key_person.rb
+++ app/models/key_person.rb
@@ -1,28 +1,31 @@
 # encoding: UTF-8
 class KeyPerson < ActiveRecord::Base
+  # Associations
   belongs_to :submission
   belongs_to :user
-  accepts_nested_attributes_for :submission, :allow_destroy => true, :reject_if => :all_blank
 
+  # Accessors
+  accepts_nested_attributes_for :submission, :allow_destroy => true, :reject_if => :all_blank
   attr_accessible *column_names
   attr_accessible :user, :submission
 
-  def self.quotes
-    "\x91\x92\x93\x94".unpack("Z*")
-  end
-  def self.right_quote
-    "\x92".unpack("Z*")[0]
-  end
+  # Validations
+  validates :role,
+            :username,
+            :first_name,
+            :last_name,
+            presence: true
 
   def uploaded_biosketch=(data_field)
     # this is simply a placeholder for updating the key_person's biosketch.
     self.user.uploaded_biosketch = data_field
   end
+
   def name
-    [first_name, last_name].join(' ').gsub(/\'/, KeyPerson.right_quote).strip
-  end
-  def sort_name
-    [last_name, first_name].join(', ').gsub(/\'/, KeyPerson.right_quote).strip
+    [first_name.strip, last_name.strip].join(' ')
   end
 
+  def sort_name
+    [last_name.strip, first_name.strip].join(', ')
+  end
 end
--- spec/models/key_person_spec.rb
+++ spec/models/key_person_spec.rb
@@ -2,16 +2,37 @@
 require 'spec_helper'
 
 describe KeyPerson, :type => :model do
-
-  it { is_expected.to belong_to(:submission) }
   it { is_expected.to belong_to(:user) }
+  it { is_expected.to belong_to(:submission) }
 
-  it 'can be instantiated' do
-    expect(FactoryGirl.build(:key_person)).to be_an_instance_of(KeyPerson)
+  context '#valid?' do
+    it { is_expected.to validate_presence_of :role }
+    it { is_expected.to validate_presence_of :username }
+    it { is_expected.to validate_presence_of :first_name }
+    it { is_expected.to validate_presence_of :last_name }
   end
 
-  it 'can be saved successfully' do
-    expect(FactoryGirl.create(:key_person)).to be_persisted
+  context '#name' do
+    it 'should combine first_name and last_name with a space' do
+      fake_key_person = KeyPerson.new(first_name: 'Fake', last_name: 'Person')
+      expect(fake_key_person.name).to eq("Fake Person")
+    end
+
+    it 'should strip leading whitespace' do
+      fake_key_person = KeyPerson.new(first_name: '   Fake', last_name: 'Person')
+      expect(fake_key_person.name).to eq("Fake Person")
+    end
+
+    it 'should strip trailing whitespace' do
+      fake_key_person = KeyPerson.new(first_name: 'Fake', last_name: 'Person   ')
+      expect(fake_key_person.name).to eq("Fake Person")
+    end
   end
 
+  context '#sort_name' do
+    it 'should put first name last, joined with a command and space' do
+      fake_key_person = KeyPerson.new(first_name: 'Fake', last_name: 'Person')
+      expect(fake_key_person.sort_name).to eq("Person, Fake")
+    end
+  end
 end
