29e59cd5d8264ba58b4208a6930f395310b8435d
jefflunt <jefflunt@gmail.com>
2014-12-23 14:06:45 -0600

    Fixed a couple things in the general mailer.

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -1,8 +1,11 @@
 class GeneralMailer < ActionMailer::Base
   default :from => "competitions@northwestern.edu"
 
-  def general_message(subject,text,recipient)
-    @text = text
-    mail(:to=>recipient,:subject=>subject)
+  def general_html_message(subject_line,recipient)
+    mail({
+      :content_type => "text/html",
+      :subject      => subject_line,
+      :to           => recipient
+    })
   end
 end
