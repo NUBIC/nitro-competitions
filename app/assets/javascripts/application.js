37bca8de799f095b6bcf57e920b06e26ca6762d4
jefflunt <jefflunt@gmail.com>
2015-01-05 16:14:10 -0600

    ASSIST-5 - Upgraded to jQuery 2.1.3. Fixed a bug in the navbar.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,11 +4,12 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require jquery
+//= require jquery.min
 //= require jquery-ui
 //= require jquery_ujs
 //= require jquery.tablesorter.min
 //= require jquery_noconflict
+//= require bootstrap.min
 //= require prototype
 //= require amstock
 //= require ddsmoothmenu
