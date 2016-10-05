ef3bf108fb1bf77b0b9662bc7a68ab94ce6331c0
Matt Baumann <matthew.baumann@northwestern.edu>
2016-10-05 09:45:36 -0500

    changed staging to nc.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,4 +6,4 @@ role :app, %w{rails-staging2.nubic.northwestern.edu}
 role :web, %w{rails-staging2.nubic.northwestern.edu}
 role :db,  %w{rails-staging2.nubic.northwestern.edu}
 
-server 'rails-staging2.nubic.northwestern.edu', user: 'mjb0760', roles: %w{web app}, primary: true
+server 'rails-staging2.nubic.northwestern.edu', user: 'nitrocompetitions', roles: %w{web app}, primary: true
