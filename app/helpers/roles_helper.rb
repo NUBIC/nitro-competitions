3d5913e67bf88b34a189a305dbe367cd76e31925
Paul Friedman <p-friedman@northwestern.edu>
2016-10-11 15:52:25 -0600

    Fix issue with project scope for reviewer menu

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -13,6 +13,7 @@ module RolesHelper
   include ApplicationHelper
   def is_admin?(program = current_program)
     return false unless session_exists?
+    return true if current_user && current_user.system_admin?
     is_logged_in? && (!session[:act_as_admin].blank? || has_role('Admin', program))
   end
 
