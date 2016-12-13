c1b620d717cb1a9d76a00e983e8a8a095820a29c
Matt Baumann <matthew.baumann@northwestern.edu>
2016-12-13 15:35:57 -0600

    Attempt to walk back commit 535c4a3. ASSIST-148

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,22 +1,22 @@
-# https://robots.thoughtbot.com/better-serialization-less-as-json
-# See above article for possible future changes. 
-class Api::ProjectsController < ApplicationController
-  skip_before_filter :authenticate_user!
+# # https://robots.thoughtbot.com/better-serialization-less-as-json
+# # See above article for possible future changes. 
+# class Api::ProjectsController < ApplicationController
+#   skip_before_filter :authenticate_user!
 
-  def index 
-    begin 
-      projects = Project.open
-      respond_to do |format|
-        format.json { render json: projects.as_json(
-          only: [:project_title, :submission_close_date, :project_url],
-          include: { program: {only: [:program_name, :program_title] } }) }
-      end
-    rescue => e
-      respond_to do |format|
-        format.json { render json: { errors: e.message}, status: :unprocessable_entity }
-      end
+#   def index 
+#     begin 
+#       projects = Project.open
+#       respond_to do |format|
+#         format.json { render json: projects.as_json(
+#           only: [:project_title, :submission_close_date, :project_url],
+#           include: { program: {only: [:program_name, :program_title] } }) }
+#       end
+#     rescue => e
+#       respond_to do |format|
+#         format.json { render json: { errors: e.message}, status: :unprocessable_entity }
+#       end
 
-    end
+#     end
 
-  end
-end
+#   end
+# end
