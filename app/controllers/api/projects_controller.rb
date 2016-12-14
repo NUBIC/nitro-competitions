e85876ef8dc6642460ae0d56cf030ceee2a3d730
Matt Baumann <matthew.baumann@northwestern.edu>
2016-12-14 14:30:53 -0600

    return project_url method to project model. ASSIST-148

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -9,7 +9,7 @@ class Api::ProjectsController < ApplicationController
         begin 
           projects = Project.open
           render json: projects.as_json(
-          only: [:project_title, :submission_close_date],
+          only: [:project_title, :submission_close_date, :project_url],
           include: { program: {only: [:program_name, :program_title] } }) 
         rescue => e
           render json: { errors: e.message}, status: :unprocessable_entity 
