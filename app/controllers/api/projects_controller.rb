5cbeea248ed8a18b87ca4b38fecaf46de24cfa60
Matt Baumann <matthew.baumann@northwestern.edu>
2016-12-14 14:24:14 -0600

    added api projects_controler. ASSIST-148

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,22 +1,20 @@
 # # https://robots.thoughtbot.com/better-serialization-less-as-json
 # # See above article for possible future changes. 
-# class Api::ProjectsController < ApplicationController
-#   skip_before_filter :authenticate_user!
+class Api::ProjectsController < ApplicationController
+  skip_before_filter :authenticate_user!
 
-#   def index 
-#     begin 
-#       projects = Project.open
-#       respond_to do |format|
-#         format.json { render json: projects.as_json(
-#           only: [:project_title, :submission_close_date, :project_url],
-#           include: { program: {only: [:program_name, :program_title] } }) }
-#       end
-#     rescue => e
-#       respond_to do |format|
-#         format.json { render json: { errors: e.message}, status: :unprocessable_entity }
-#       end
-
-#     end
-
-#   end
-# end
+  def index 
+    respond_to do |format|
+      format.json do
+        begin 
+          projects = Project.open
+          render json: projects.as_json(
+          only: [:project_title, :submission_close_date],
+          include: { program: {only: [:program_name, :program_title] } }) 
+        rescue => e
+          render json: { errors: e.message}, status: :unprocessable_entity 
+        end
+      end
+    end
+  end
+end
