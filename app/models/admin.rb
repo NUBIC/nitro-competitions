1b10a97385a4ae5098d7b57b2da7810a2a592225
Matt Baumann <matthew.baumann@northwestern.edu>
2018-01-30 14:35:45 -0600

    remove unused admin model. ASSIST-229

--- app/models/admin.rb
+++
@@ -1,9 +0,0 @@
-# encoding: UTF-8
-class Admin < ActiveRecord::Base
-  # each admin is a user (but through a user_id) and belongs to a program
-  belongs_to :program
-  belongs_to :user
-
-  attr_accessor :reviewer_list
-
-end
