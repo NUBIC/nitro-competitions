5e1a424e71a6f7a6036eeba495d99cd751986928
jseraf <john.d.serafin@gmail.com>
2018-03-26 11:48:47 -0500

    standardize the order of scores in submission_review model

--- app/models/submission_review.rb
+++ app/models/submission_review.rb
@@ -11,19 +11,27 @@ class SubmissionReview < ActiveRecord::Base
   scope :this_project, lambda { |*args| joins(:submission).where('submissions.project_id = :project_id', { :project_id => args.first }) }
   scope :active, lambda { |*args| joins(:submission).where('submissions.project_id IN (:project_ids)', { :project_ids => args.first }) }
 
-  validates_numericality_of :innovation_score, :allow_nil => true, :only_integer => true, :less_than_or_equal_to => 9, :greater_than_or_equal_to => 0
   validates_numericality_of :impact_score, :allow_nil => true, :only_integer => true, :less_than_or_equal_to => 9, :greater_than_or_equal_to => 0
-  validates_numericality_of :scope_score, :allow_nil => true, :only_integer => true, :less_than_or_equal_to => 9, :greater_than_or_equal_to => 0
   validates_numericality_of :team_score, :allow_nil => true, :only_integer => true, :less_than_or_equal_to => 9, :greater_than_or_equal_to => 0
+  validates_numericality_of :innovation_score, :allow_nil => true, :only_integer => true, :less_than_or_equal_to => 9, :greater_than_or_equal_to => 0
+  validates_numericality_of :scope_score, :allow_nil => true, :only_integer => true, :less_than_or_equal_to => 9, :greater_than_or_equal_to => 0
   validates_numericality_of :environment_score, :allow_nil => true, :only_integer => true, :less_than_or_equal_to => 9, :greater_than_or_equal_to => 0
-  validates_numericality_of :other_score, :allow_nil => true, :only_integer => true, :less_than_or_equal_to => 9, :greater_than_or_equal_to => 0
   validates_numericality_of :budget_score, :allow_nil => true, :only_integer => true, :less_than_or_equal_to => 9, :greater_than_or_equal_to => 0
+  validates_numericality_of :other_score, :allow_nil => true, :only_integer => true, :less_than_or_equal_to => 9, :greater_than_or_equal_to => 0
   validates_numericality_of :completion_score, :allow_nil => true, :only_integer => true, :less_than_or_equal_to => 9, :greater_than_or_equal_to => 0
+
   validates_numericality_of :overall_score, :only_integer => true, :less_than_or_equal_to => 9, :greater_than_or_equal_to => 0
 
   def composite_score
     return 0 if unscored?
-    (((z(innovation_score) + z(impact_score) + z(scope_score) + z(team_score) + z(environment_score) + z(budget_score) + z(other_score) + z(completion_score)).to_f / count_nonzeros?) * 10).round / 10.0
+    (((z(impact_score) +
+       z(team_score) +
+       z(innovation_score) +
+       z(scope_score) +
+       z(environment_score) +
+       z(budget_score) +
+       z(other_score) +
+       z(completion_score)).to_f / count_nonzeros?) * 10).round / 10.0
   end
 
   def z(val)
@@ -31,18 +39,22 @@ class SubmissionReview < ActiveRecord::Base
   end
 
   def has_zero?
-    z?(innovation_score) || z?(impact_score) || z?(scope_score) || z?(team_score) || z?(environment_score)
+    z?(impact_score) ||
+    z?(team_score) ||
+    z?(innovation_score) ||
+    z?(scope_score) ||
+    z?(environment_score)
   end
 
   def count_nonzeros
-    nz?(innovation_score) +
     nz?(impact_score) +
-    nz?(scope_score) +
     nz?(team_score) +
+    nz?(innovation_score) +
+    nz?(scope_score) +
     nz?(environment_score) +
     nz?(budget_score) +
-    nz?(completion_score) +
-    nz?(other_score)
+    nz?(other_score) +
+    nz?(completion_score)
   end
   alias :count_nonzeros? :count_nonzeros
 
