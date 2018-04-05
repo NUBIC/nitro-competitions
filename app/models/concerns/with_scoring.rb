90a28bd45eb536b196c2676e777a8db1c93ec675
jseraf <john.d.serafin@gmail.com>
2018-04-05 15:12:11 -0500

    move scoring criteria and average calculation to with_scoring concern

---
+++ app/models/concerns/with_scoring.rb
@@ -0,0 +1,22 @@
+module WithScoring
+  extend ActiveSupport::Concern
+
+  # NIH research criteria
+  DEFAULT_CRITERIA    = ['impact',
+                         'team',
+                         'innovation',
+                         'scope',
+                         'environment'].freeze
+
+  ADDITIONAL_CRITERIA = ['budget',
+                         'other',
+                         'completion'].freeze
+
+  # Combined criteria based on Project attributes
+  CRITERIA = (DEFAULT_CRITERIA + ADDITIONAL_CRITERIA).freeze
+
+  def calculate_average scores
+    return 0 if scores.empty?
+    (scores.sum.to_f / scores.size).round(2)
+  end
+end
