d826a7c79b3a42b3384e554709b5713525b5f55d
jseraf <john.d.serafin@gmail.com>
2018-05-23 13:09:44 -0500

    clear duplicated rfa_url when it links to orig project. ASSIST-257

--- app/services/project_services/duplicate.rb
+++ app/services/project_services/duplicate.rb
@@ -2,10 +2,17 @@ module ProjectServices
   module Duplicate
     def self.call(project_id)
       @project = Project.find(project_id).dup
+      update_rfa_url if @project.rfa_url.match? project_id
       @project.write_attribute(:visible, false)
       @project.tap { |project| project.attribute_names.
         select{ |name| name.match? '_date' }.
           each{ |date| @project.write_attribute(date.to_sym, nil ) } }
     end
+
+    private
+    def self.update_rfa_url
+      @project.write_attribute(:rfa_url, '')
+    end
+
   end
 end
