9fd9c17e274447c0b508aa55d6596d97dbde49b8
Matt Baumann <matthew.baumann@northwestern.edu>
2018-09-06 17:15:57 -0500

    Another test on deploy tasks. ASSIST-126.

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,9 +46,14 @@ namespace :deploy do
     task :database_config do
       on roles(:web) do
         execute "ln -nfs /etc/nubic/db/nitro_competitions.yml #{release_path}/config/database.yml"
+      end
+    end
+    task :ldap_config do
+      on roles(:web) do
         execute "ln -nfs /etc/nubic/ldap.yml #{release_path}/config/ldap.yml"
       end
     end
   end
   before 'deploy:assets:precompile', 'deploy:symlink:database_config'
+  after 'deploy:symlink:database_config'
 end
